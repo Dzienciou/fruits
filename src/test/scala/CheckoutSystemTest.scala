550f817c3abe75fcad2f4615603ccd16bec76a12
Dzienciou <mikolaj.dzieciolowski@gmail.com>
2019-09-11 22:28:56 +0200

    test 2: only apples list

--- src/test/scala/CheckoutSystemTest.scala
+++ src/test/scala/CheckoutSystemTest.scala
@@ -2,7 +2,10 @@ class CheckoutSystemTest extends org.specs2.mutable.Specification {
 
   "CheckoutSystem" should {
     "return 0.0 for empty list" in {
-      CheckoutSystem.compute(Seq.empty[String]) must_== BigDecimal(0.0)
+      CheckoutSystem.compute(Seq.empty[String]) must_== BigDecimal("0.0")
+    }
+    "return correct value for list of Apples" in {
+      CheckoutSystem.compute(Seq("Apple", "Apple", "Apple")) must_== BigDecimal("1.8")
     }
   }
 
